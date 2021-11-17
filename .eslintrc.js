e4ba2cf0222faf9e10e38070efbf03545b45c0e1
Pavel Sturov <pavel.sturau@winlinetech.net>
2021-11-17 20:49:36 +0300

    Updated eslint rules. Implemented models for db.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,9 @@ module.exports = {
     project:    'tsconfig.json',
     sourceType: 'module',
   },
-  plugins:        ['@typescript-eslint/eslint-plugin'],
+  plugins:        [
+    '@typescript-eslint/eslint-plugin',
+  ],
   extends:        [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -15,14 +17,23 @@ module.exports = {
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
-  rules: {
+  rules:          {
+    '@typescript-eslint/no-empty-interface':             'off',
     '@typescript-eslint/interface-name-prefix':          'off',
     '@typescript-eslint/explicit-function-return-type':  'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any':                'off',
     '@typescript-eslint/no-empty-function':              'off',
     'semi':                                              ['error', 'never'],
-    'indent':                                            ['error', 2],
+    'indent':                                            [
+      'error', 2, {
+        'ignoredNodes': [
+          'FunctionExpression > .params[decorators.length > 0]',
+          'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
+          'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
+        ],
+      },
+    ],
     'key-spacing':                                       ['error', { 'mode': 'minimum', 'align': 'value' }],
     'prettier/prettier':                                 ['off', { endOfLine: 'auto' }],
   },
